e8ada2d1506201ef929d87ae3434ef60a3c3f0bc
Dario <1367wm2@gmail.com>
2023-07-26 16:05:08 -0300

    Modified structure to fit new module

--- basic_qc.py
+++ basic_qc.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 from datetime import datetime
 import pandas as pd
-from utils import *
+from qcontrol import *
+from shadowband import *
 
 '''
 Script para control de calidad (Quality Control) básico de datos solares.
@@ -11,22 +12,35 @@ Este script puede:
 # Parametro para medir el tiempo de corrida del programa
 tic = datetime.now()
 
-
 # Parámetros de geolocalización
-year = 2021
 latitud = -32.898
 longitud = -68.875
+longitud_std = -45  # Longitud estándar 0 a 360 hacia el oeste (Longitud del lugar de referencia)
 altitud = 842  # msnm
 b = 7.5  # Ancho de la banda (cm)
 r = 30.8  # Radio de la banda (cm)
 
 # Carga del archivo a analizar
-file_path = f"data/{year}-minute-raw.csv"
+file_path = f"data/2022-minute.csv"
 file_df = pd.read_csv(file_path, sep=";")
+# Parseo y reordenamiento del archivo cargado
 file_df['fecha'] = pd.to_datetime(file_df['fecha'], format="%d/%m/%Y %H:%M")
-file_df["solartime"] = [standard2solar_time_modified(date, lng=abs(longitud), lng_std=45) for date in file_df.fecha]
+file_df["solartime"] = [standard2solar_time_modified(date, lng=abs(longitud),
+                                                     lng_std=abs(longitud_std)) for date in file_df.fecha]
 file_df.sort_values(by=["fecha"], inplace=True)
 file_df.reset_index(inplace=True)
+
+# Cálculo de coeficietes de corrección de difusa por medio de LeBaron
+# measurements = [SolarMeasurement(date, ghi, dif, latitud, longitud, longitud_std, altitud, b, r) for date, ghi, dif in
+#                 zip(file_df["fecha"], file_df["IRGLO"], file_df["IRDIF"])]
+measurements_series = pd.Series(map(lambda date, ghi, dif: SolarMeasurement(date, ghi, dif, lat=latitud, lng=longitud,
+                                                                            lng_std=longitud_std, altitude=altitud,
+                                                                            shadowband_width=b, shadowband_radius=r),
+                                    file_df["fecha"], file_df["IRGLO"], file_df["IRDIF"]))
+# lebaron_list = [lebaron(measurement) for measurement in measurements]
+lebaron_series = pd.Series(map(lambda measurement: lebaron(measurement), measurements_series))
+
+# ---------- Control de calidad ----------
 
 # Chequeo de la integridad de los timestamp
 # complete_timestamps = set(pd.date_range(start=f"{year}-01-01 00:00", end=f"{year}-12-31 23:59", freq="min"))
@@ -38,51 +52,19 @@ file_df.reset_index(inplace=True)
 # full_df = pd.concat([file_df, missing_df]).sort_values(by=["fecha"])
 # full_df.reset_index()
 
-# Cálculo LeBaron
-
-# zenith_angle_series_rad = [sp.theta_z(date, lat=latitud) for date in file_df.solartime]
-# gon_series = [sp.gon(date) for date in file_df.solartime]
-# dni_series = [dir_nu(irglo, irdif, theta_z_value) for irglo, irdif, theta_z_value in zip(file_df.IRGLO, file_df.IRDIF,
-#                                                                                          zenith_angle_series_rad)]
-# epsilon_series = [epsilon(irdif, dni_value, date) for irdif, dni_value, date in zip(file_df.IRDIF, dni_series,
-#                                                                                     file_df.solartime)]
-# delta_series = [delta(irdif, theta_z_value, gon_value) for irdif, theta_z_value, gon_value in
-#                 zip(file_df.IRDIF, zenith_angle_series_rad, gon_series)]
-# c_i_series = [c_i(date) for date in file_df.solartime]
-# dff = pd.DataFrame({"theta_z": zenith_angle_series_rad, "gon": gon_series, "dni": dni_series,
-#                     "epsilon": epsilon_series, "delta": delta_series, "c_i": c_i_series})
-
-zenith_angle_series_rad = file_df.solartime.apply(lambda date: sp.theta_z(date, lat=latitud))
-zenith_angle_series_deg = np.rad2deg(zenith_angle_series_rad)
-gon_series = file_df.solartime.apply(sp.gon)
-dni_series = pd.Series(map(lambda irglo, irdif, theta_z_value: dir_nu(irglo, irdif, theta_z_value),
-                           file_df.IRGLO, file_df.IRDIF, zenith_angle_series_rad))
-epsilon_series = pd.Series(map(lambda irdif, dni_value, date: epsilon(irdif, dni_value, date, latitud), file_df.IRDIF,
-                               dni_series, file_df.solartime))
-delta_series = pd.Series(map(lambda irdif, theta_z_value, gon_value: delta(irdif, theta_z_value, gon_value, altitud),
-                             file_df.IRDIF, zenith_angle_series_rad, gon_series))
-c_i_series = pd.Series(map(lambda date: c_i_original(date, latitud=latitud, shadowband_width=b, shadowband_radius=r),
-                           file_df.solartime))
-sunrise_time_series = pd.Series(map(lambda date: sp.sunrise_time(date, lat=latitud), file_df.solartime))
-
-cuts = pd.Series(map(lambda aa, bb, cc, dd: cut(aa, bb, cc, dd), zenith_angle_series_deg, c_i_series, epsilon_series,
-                     delta_series))
-correction_series = pd.Series(map(lambda cut_input: lebaron(cut_input), cuts))
-dif_corrected = file_df.IRDIF * correction_series
-
-# Límites físicos y extremadamente raros
-ghi = file_df.loc[:, ["fecha", "IRGLO", "solartime"]]
-dhi = file_df.loc[:, ["fecha", "IRDIF", "solartime"]]
-factor_series = pd.Series(map(lambda gon_value, theta_z_value: gon_factor(gon_value, theta_z_value), gon_series,
-                              zenith_angle_series_rad))
-ghi["upper_physical_limit"] = 1.5 * factor_series + 100
-ghi["upper_extreme_limit"] = 1.2 * factor_series + 50
-dhi["upper_physical_limit"] = 0.95 * factor_series + 50
-dhi["upper_extreme_limit"] = 0.75 * factor_series + 30
-physical_lower_limit = -4
-extreme_lower_limit = -2
-ghi.index = ghi.fecha
-dhi.index = dhi.fecha
+# # Límites físicos y extremadamente raros
+# ghi = file_df.loc[:, ["fecha", "IRGLO", "solartime"]]
+# dhi = file_df.loc[:, ["fecha", "IRDIF", "solartime"]]
+# factor_series = pd.Series(map(lambda gon_value, theta_z_value: gon_factor(gon_value, theta_z_value), gon_series,
+#                               zenith_angle_series_rad))
+# ghi["upper_physical_limit"] = 1.5 * factor_series + 100
+# ghi["upper_extreme_limit"] = 1.2 * factor_series + 50
+# dhi["upper_physical_limit"] = 0.95 * factor_series + 50
+# dhi["upper_extreme_limit"] = 0.75 * factor_series + 30
+# physical_lower_limit = -4
+# extreme_lower_limit = -2
+# ghi.index = ghi.fecha
+# dhi.index = dhi.fecha
 
 # Fin del programa: se ejecuta parte final para conteo de corrida
 toc = datetime.now()
