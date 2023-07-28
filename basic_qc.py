650a1b950fea0a2f7842663507e3fecc6ab624e3
Dario <1367wm2@gmail.com>
2023-07-28 15:53:31 -0300

    Corrected headline, and parameters description. Removed useless lines and optimized objects creation.

--- basic_qc.py
+++ basic_qc.py
@@ -5,17 +5,16 @@ from qcontrol import *
 from shadowband import *
 
 '''
-Script para control de calidad (Quality Control) básico de datos solares.
-Este script puede:
-    -Verificar integridad de los datos (Que no existan periodos donde no se haya hecho la adquisición de datos)
+Script para corregir los datos de radiación difusa tomados con banda de sombra. Se emplea LeBaron para la corrección.
 '''
+
 # Parametro para medir el tiempo de corrida del programa
 tic = datetime.now()
 
 # Parámetros de geolocalización
-latitud = -32.898   # Latitud del lugar (de -180 a 180, oeste negativo)
-longitud = -68.875  # Longitud del lugar (de -90 a 90, sur negativo)
-longitud_std = -45  # Longitud estándar 0 a 360 hacia el oeste (Longitud del meridiano del lugar de referencia)
+latitud = -32.898   # Latitud del lugar (de -90 a 90, sur negativo)
+longitud = -68.875  # Longitud del lugar (de -180 a 180, oeste negativo)
+longitud_std = -45  # Longitud estándar (de -180 a 180, oeste negativo). Longitud del meridiano del lugar de referencia
 altitud = 842  # msnm
 b = 7.5  # Ancho de la banda (cm)
 r = 30.8  # Radio de la banda (cm)
@@ -24,24 +23,27 @@ r = 30.8  # Radio de la banda (cm)
 file_path = f"data/2022-minute.csv"
 file_df = pd.read_csv(file_path, sep=";")
 # Parseo y reordenamiento del archivo cargado
+# file_df["fecha"] = file_df.fecha.map(lambda fecha: pd.to_datetime(fecha, format="%d/%m/%Y %H:%M"))
 file_df['fecha'] = pd.to_datetime(file_df['fecha'], format="%d/%m/%Y %H:%M")
-file_df["solartime"] = [standard2solar_time_modified(date, lng=abs(longitud),
-                                                     lng_std=abs(longitud_std)) for date in file_df.fecha]
 file_df.sort_values(by=["fecha"], inplace=True)
 file_df.reset_index(inplace=True)
 
 # Cálculo de coeficietes de corrección de difusa por medio de LeBaron
-measurements_series = pd.Series(map(lambda date, ghi, dif: SolarMeasurement(date, ghi, dif, lat=latitud, lng=longitud,
-                                                                            lng_std=longitud_std, altitude=altitud,
-                                                                            shadowband_width=b, shadowband_radius=r),
-                                    file_df["fecha"], file_df["IRGLO"], file_df["IRDIF"]))
-lebaron_series = measurements_series.map(lambda measurement: measurement.dif_correction_factor)
+measurements_list = [SolarMeasurement(date, ghi, dif, lat=latitud, lng=longitud, lng_std=longitud_std, altitude=altitud,
+                                      shadowband_width=b, shadowband_radius=r) for date, ghi, dif
+                     in zip(file_df["fecha"], file_df["IRGLO"], file_df["IRDIF"])]
+lebaron_list = [solar_measurement.dif_correction_factor for solar_measurement in measurements_list]
 
-#
+# measurements_series = pd.Series(map(lambda date, ghi, dif: SolarMeasurement(date, ghi, dif, lat=latitud, lng=longitud,
+#                                                                             lng_std=longitud_std, altitude=altitud,
+#                                                                             shadowband_width=b, shadowband_radius=r),
+#                                     file_df["fecha"], file_df["IRGLO"], file_df["IRDIF"]))
+# lebaron_series = measurements_series.map(lambda measurement: measurement.dif_correction_factor)
+
 # ---------- Control de calidad ----------
 
 # Chequeo de la integridad de los timestamp
-# complete_timestamps = set(pd.date_range(start=f"{year}-01-01 00:00", end=f"{year}-12-31 23:59", freq="min"))
+# complete_timestamps = set(pd.date_range(start=f"2022-01-01 00:00", end=f"2022-12-31 23:59", freq="min"))
 # file_timestamps = set(file_df.fecha)
 # missing_timestamps = complete_timestamps.difference(file_timestamps)
 # print(f"Faltan {len(missing_timestamps)} de {len(complete_timestamps)} timestamps")
